4c43b2c7131286de816d1be8c4da72e688f37865
Dakar13 <ecalvamtz@gmail.com>
2020-05-24 13:18:11 -0500

    Error de compilacion corregido con @types/express

---
+++ frontend/src/server.ts
@@ -0,0 +1,56 @@
+// Dependencies
+import express from 'express'
+import next from 'next'
+// import path from 'path'
+// import bodyParser from 'body-parser'
+// import cookieParser from 'cookie-parser'
+// import cors from 'cors'
+// import session from 'express-session'
+/*
+// Configuration
+import config from '@config'
+
+// Middlewares
+import user, { isConnected } from '@middlewares/user'
+*/
+// Settings up Next App
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
+
+// Running Next App
+nextApp.prepare().then(() => {
+  const app = express()
+  /*
+    // Public static
+    app.use(express.static(path.join(__dirname, '../public')))
+
+    // Middlewares
+    app.use(session({
+      resave: false,
+      saveUninitialized: true,
+      secret: config.security.secretKey
+    }))
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(cookieParser(config.security.secretKey))
+    app.use(cors({ credentials: true, origin: true }))
+    app.use(user)
+
+    // Routes
+    app.get('/login', isConnected(false), (req, res) => {
+        return nextApp.render(req, res, '/users/login', req.query)
+    })
+
+    app.get('/dashboard', isConnected(true, 'god', '/login?redirectTo=/dashboard'), (req, res) => {
+      return nextApp.render(req, res, '/dashboard', req.query)
+    })
+*/
+  app.all('*', (req: any, res: any) => {
+    return handle(req, res)
+  })
+
+  // Listening port 3000
+  // app.listen(config.serverPort)
+  app.listen(3000)
+})

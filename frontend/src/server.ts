73ec14a0f6c935f170585f6d4629149af0541e21
Dakar13 <ecalvamtz@gmail.com>
2020-05-24 18:54:02 -0500

    Alias y Login UI

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,7 +1,7 @@
 // Dependencies
 import express from 'express'
 import next from 'next'
-// import path from 'path'
+import path from 'path'
 // import bodyParser from 'body-parser'
 // import cookieParser from 'cookie-parser'
 // import cors from 'cors'
@@ -21,31 +21,32 @@ const handle = nextApp.getRequestHandler()
 // Running Next App
 nextApp.prepare().then(() => {
   const app = express()
-  /*
-    // Public static
-    app.use(express.static(path.join(__dirname, '../public')))
 
-    // Middlewares
-    app.use(session({
-      resave: false,
-      saveUninitialized: true,
-      secret: config.security.secretKey
-    }))
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(cookieParser(config.security.secretKey))
-    app.use(cors({ credentials: true, origin: true }))
-    app.use(user)
+  // Public static
+  app.use(express.static(path.join(__dirname, '../public')))
 
-    // Routes
-    app.get('/login', isConnected(false), (req, res) => {
-        return nextApp.render(req, res, '/users/login', req.query)
-    })
+  // // Middlewares
+  // app.use(session({
+  //   resave: false,
+  //   saveUninitialized: true,
+  //   secret: config.security.secretKey
+  // }))
+  // app.use(bodyParser.json())
+  // app.use(bodyParser.urlencoded({ extended: false }))
+  // app.use(cookieParser(config.security.secretKey))
+  // app.use(cors({ credentials: true, origin: true }))
+  // app.use(user)
 
-    app.get('/dashboard', isConnected(true, 'god', '/login?redirectTo=/dashboard'), (req, res) => {
-      return nextApp.render(req, res, '/dashboard', req.query)
-    })
-*/
+  // Routes
+  // app.get('/login', isConnected(false), (req, res) => {
+  app.get('/login', (req: any, res: any) => {
+    return nextApp.render(req, res, '/users/login', req.query)
+  })
+
+  // app.get('/dashboard', isConnected(true, 'god', '/login?redirectTo=/dashboard'), (req, res) => {
+  //   return nextApp.render(req, res, '/dashboard', req.query)
+  // })
+
   app.all('*', (req: any, res: any) => {
     return handle(req, res)
   })
